eb60935cdf69630a4f5341f2980a08541977874a
deepblue <deepblue@inha.ac.kr>
2024-01-15 10:44:14 +0900

    v0.3 python day02

---
+++ day02.py
@@ -0,0 +1,19 @@
+univ = "Inha university"
+print(univ)
+print(univ[5])
+# univ[5] = 'U'  # immutable
+# print(univ)
+subjects = ['python', 'c++', 'linux', 'data structure', 'database']
+print(subjects)
+print(subjects[3])
+subjects[3] = 'data structure & algorithm'  # mutable
+print(subjects)
+
+# print(0.1)
+# print(1e-1)
+# print(0.01)
+# print(1e-2)
+# print(314.1592)
+# print(0.3141592e3)
+# print(21000)
+# print(21_000)
