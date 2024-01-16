7f1dde3fd4db34bba67609ecc0b6a077322fa09c
deepblue <deepblue@inha.ac.kr>
2024-01-16 11:44:44 +0900

    v2.0 day03

--- day03.py
+++ day03.py
@@ -1,8 +1,9 @@
+# preview
 subjects = "python c++ database linux"
 subject = input("수강신청과목 입력 : ")
-if subjects.find(subject) != -1:
-    print(f"해당 과목이 존재합니다. 위치는 {subjects.find(subject)}번 인덱스입니다.")
-else:
+try:
+    print(f"해당 과목이 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.")
+except ValueError:
     print('해당과목이 존재하지 않습니다')
 
 
