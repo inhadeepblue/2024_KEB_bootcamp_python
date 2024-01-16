29451eff99af55a008c4ca054efa0aca7219f26a
deepblue <deepblue@inha.ac.kr>
2024-01-16 14:11:01 +0900

    v2.4 prime number day03

--- day03.py
+++ day03.py
@@ -5,7 +5,10 @@ i = 2
 while i < number:
     if number % i == 0:
         cnt = cnt + 1
+        break
+    #print(i, end=' ')
     i = i + 1
+
 
 if cnt == 0:
     print(f'{number} is prime number')
