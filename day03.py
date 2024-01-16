5fa69f45d19124d3357673b6348de7e412fcff5d
deepblue <deepblue@inha.ac.kr>
2024-01-16 13:36:44 +0900

    v2.1 day03

--- day03.py
+++ day03.py
@@ -1,5 +1,7 @@
 # preview
 subjects = "python c++ database linux"
+print('%e' % 0.7045)
+print(subjects.isalnum())
 subject = input("수강신청과목 입력 : ")
 try:
     print(f"해당 과목이 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.")
