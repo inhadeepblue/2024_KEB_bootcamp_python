d9546987cc3ec92b0b85acb499d75f07f33f804e
deepblue <deepblue@inha.ac.kr>
2024-01-17 09:09:25 +0900

    v3.1 assignment day04

---
+++ day04.py
@@ -0,0 +1,20 @@
+# Assignment
+numbers = input("Input first second number : ").split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
+
+if n1 > n2:
+    n1, n2 = n2, n1
+
+for number in range(n1, n2+1):
+    is_prime = True
+
+    if number < 2:
+        #pass
+        continue
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                is_prime = False
+                break
+        if is_prime: print(number, end=' ')
